9a76006b6238a1adb33cabc630bcbdb9839a48ec
Bruno Minatel <brunohrq_@hotmail.com>
2026-06-24 03:12:55 -0300

    sistema de maior e menor numero

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,49 @@
 import java.util.Scanner;
-public class Main{
+
+public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um numero aleatorio");
-        int numero = scanner.nextInt();
 
-        if (numero %2 !=0){
-            System.out.println("Ele é um numero impar");
+        System.out.println("Digite o Primeiro Numero");
+        long numero1 = scanner.nextLong();
+
+        System.out.println("Digite o Segundo Numero");
+        long numero2 = scanner.nextLong();
+
+        System.out.println("Digite o Terceiro Numero");
+        long numero3 = scanner.nextLong();
+
+        System.out.println("Digite o Quarto Numero");
+        long numero4 = scanner.nextLong();
+
+        long maior = numero1;
+        long menor = numero1;
+
+        if (numero2 < menor) {
+            menor = numero2;
         }
-        else {
-            System.out.println("Ele é um numero par");
+        if (numero2 > maior) {
+            maior = numero2;
         }
+
+        if (numero3 < menor) {
+            menor = numero3;
+        }
+        if (numero3 > maior) {
+            maior = numero3;
+        }
+
+        if (numero4 < menor) {
+            menor = numero4;
+        }
+        if (numero4 > maior) {
+            maior = numero4;
+        }
+
+        System.out.println("Maior numero é " + maior);
+        System.out.println("Menor numero é " + menor);
+
         scanner.close();
     }
 }
