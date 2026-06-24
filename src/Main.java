31d290629fdd858fb4cbd7114ee304e82eab2cf7
Bruno Minatel <brunohrq_@hotmail.com>
2026-06-24 02:41:39 -0300

    criado um sistema de identificacao de numero impares e pares

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,17 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
-
 public class Main{
-    public static void main (String []args){
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero aleatorio");
+        int numero = scanner.nextInt();
 
-
-
-
-    Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Digite sua nota de Matematica");
-        double notaMatematica = scanner.nextDouble();
-        System.out.println("Digite sua nota de Portugues");
-        double notaPortugues = scanner.nextDouble();
-        System.out.println("Digite sua nota de Historia");
-        double notaHistoria = scanner.nextDouble();
-        System.out.println("Digite sua nota de Geografia");
-        double notaGeografia = scanner.nextDouble();
-        System.out.println("Digite o numero de faltas que teve esse ano");
-        double numeroFaltas = scanner.nextDouble();
-
-        double media = (notaGeografia + notaHistoria + notaMatematica + notaPortugues) /4;
-        if (numeroFaltas >=100){
-            System.out.println("Reprovado por falta");
-        }
-        else if (media >=7.0){
-            System.out.println("Voce foi Aprovado " + "sua media foi " + media);
-        }
-        else if (media >=5 && media <7) {
-            System.out.println("Voce esta em Recuperacao " + "sua media foi " + media);
+        if (numero %2 !=0){
+            System.out.println("Ele é um numero impar");
         }
         else {
-            System.out.println("Voce foi Reprovado " + "Sua media foi " + media);
+            System.out.println("Ele é um numero par");
         }
-
-       scanner.close();
-
-
-
-
+        scanner.close();
     }
 }
