44c01f2434c4bbe94786d7af3b1cb6c2c4629824
Bruno Minatel <brunohrq_@hotmail.com>
2026-06-24 02:09:36 -0300

    criado um sistema de media de aluno

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,38 @@
 import java.util.Scanner;
+
 public class Main{
-    public static void main(String [] args){
-        Scanner scanner = new Scanner (System.in);
-        System.out.println("How much do you earn by month?");
-        double salary = scanner.nextDouble();
-        if (salary <=599.){
-            System.out.println("You earn less then 600,00 per month");
+    public static void main (String []args){
+
+
+
+
+
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua nota de Matematica");
+        double notaMatematica = scanner.nextDouble();
+        System.out.println("Digite sua nota de Portugues");
+        double notaPortugues = scanner.nextDouble();
+        System.out.println("Digite sua nota de Historia");
+        double notaHistoria = scanner.nextDouble();
+        System.out.println("Digite sua nota de Geografia");
+        double notaGeografia = scanner.nextDouble();
+        double media = (notaGeografia + notaHistoria + notaMatematica + notaPortugues) /4;
+
+        if (media >=7.0){
+            System.out.println("Voce foi Aprovado " + "sua media foi " + media);
         }
-        else if (salary >= 600. && salary <= 1000.){
-            System.out.println("You earn between 600 to 1000 Dol per month");
+        else if (media >=5 && media <7) {
+            System.out.println("Voce esta em Recuperacao " + "sua media foi " + media);
         }
-        else if(salary >=1000. && salary <=2000.){
-            System.out.println("You earn between 1000 to 2000 Dol per month");
+        else {
+            System.out.println("Voce foi Reprovado " + "Sua media foi " + media);
         }
-        else if (salary >2000. && salary <=3500.){
-            System.out.println("You earn Between 2000 to 3500 Dol per month");
-        }
-        else if (salary >3500. && salary <=5500.){
-            System.out.println("You earn Between 3500 to 5500 Dol per month");
-        }
-        else
-        {
-            System.out.println("You earn more then 5500,00 Dolars per month");
-        }
-        scanner.close();
+
+       scanner.close();
+
+
 
 
     }
 }
-
-
-
-
