b6d2b3df3da564bdd1fb32b9a2befc135a4a35de
Bruno Minatel <brunohrq_@hotmail.com>
2026-06-24 01:17:28 -0300

    adicionado mais salario

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String [] args){
-       Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner (System.in);
         System.out.println("How much do you earn by month?");
-       double salary = scanner.nextDouble();
-       if (salary <=1999.){
-           System.out.println("You earn less then 2.000,00 per month");
-       }
-       else if (salary >= 2000. && salary <= 3000.){
-           System.out.println("You earn between 2000 to 3000 Dol per month");
-       }
-       else
-       {
-           System.out.println("You earn more then 300000 Dolars per month");
-       }
-       scanner.close();
+        double salary = scanner.nextDouble();
+        if (salary <=599.){
+            System.out.println("You earn less then 600,00 per month");
+        }
+        else if (salary >= 600. && salary <= 1000.){
+            System.out.println("You earn between 600 to 1000 Dol per month");
+        }
+        else if(salary >=1000. && salary <=2000.){
+            System.out.println("You earn between 1000 to 2000 Dol per month");
+        }
+        else if (salary >2000. && salary >=3500.){
+            System.out.println("You earn Between 2000 to 3500 Dol per month");
+        }
+        else if (salary >3500. && salary >=5500.){
+            System.out.println("You earn Between 3500 to 5500 Dol per month");
+        }
+        else
+        {
+            System.out.println("You earn more then 5500,00 Dolars per month");
+        }
+        scanner.close();
 
 
     }
 }
+
+
+
+
