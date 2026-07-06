649ea081f56a1e267ae5a399321ee8664634d17c
Bruno Minatel <brunohrq_@hotmail.com>
2026-07-06 00:11:14 -0300

    calculadora

--- src/MenoreMaior.java
+++ src/MenoreMaior.java
@@ -1,66 +1,16 @@
-import javax.swing.*;
-import  java.util.Scanner;
-public class MenoreMaior{
+import java.util.Scanner;
+
+public class MenoreMaior {
     public static void main(String[] args) {
 
 
-            boolean rodarAp = true;
-            double altura = 0;
-            double peso = 0;
-            double imC = 0;
-            String classificacao = "";
-
         Scanner sc = new Scanner(System.in);
-            while (rodarAp){
+        System.out.println("Digite um numero de 1 a 10");
 
-            String nome = JOptionPane.showInputDialog(null,"Digite seu nome");
+        int numero = sc.nextInt();
+        for (int i = 1; i <=numero; i++){
+            System.out.println(numero + "  X  " + i + " = " + (i * numero));
+        }
 
-            peso = Double.parseDouble(JOptionPane.showInputDialog(nome + " Digite seu peso"));
-                    if (peso <0 ) {
-                        JOptionPane.showMessageDialog(null, "Peso invalido, refaça a operação");
-                        continue;
-                    } else {
-                        JOptionPane.showMessageDialog(null, "seu peso é " + peso + " kg");
-                        }
-
-                     altura = Double.parseDouble(JOptionPane.showInputDialog(null,nome + "Digite sua altura"));
-
-                    if (altura <=0) {
-                        JOptionPane.showMessageDialog(null, "Altura invalida, refaça a operação");
-                        continue;
-                    } else {
-                        JOptionPane.showMessageDialog(null,"sua altura é de " +altura + " m");
-                    }
-
-                    imC = peso / (altura * altura);
-                    JOptionPane.showMessageDialog(null,imC);
-
-                        if (imC <=18.5){
-                       classificacao = "Abaido do Peso";
-
-
-                        } else if (imC <= 24.9){
-                            classificacao = "Peso Ideal";
-
-
-                        } else if (imC <= 29.9){
-                            classificacao = "Sobrepeso";
-
-
-                        } else {
-                            classificacao = "Obessidade";
-                        }
-
-                        JOptionPane.showMessageDialog(null, "Nome:" + nome + "\n"
-                        + "Peso:" + peso + "\n" + "Altura:" + altura + "\n" + imC + "\n" + "Classificaçao:" + classificacao);
-
-                        int continuar = Integer.parseInt(JOptionPane.showInputDialog(null,"Deseja refazer a Opcao?" + "\n" + "Opcao 1: SIM \n" + "Opcao 2: NAO \n"));
-                        if (continuar !=2){
-                            ;continue;
-                        } else {
-                            return;
-                        }
     }
-
-}}
-
+}
