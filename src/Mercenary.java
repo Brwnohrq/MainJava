031b185917cfc71805d9ec69673031a57c79cc85
Bruno Minatel <brunohrq_@hotmail.com>
2026-08-11 22:32:19 -0300

    flyff com poo, inicio

--- src/Mercenary.java
+++ src/Mercenary.java
@@ -3,15 +3,21 @@ public class Warrior extends Character {
 
     private int rage;
     private String weapon;
-
+    private int lifeWeapon;
+    private int startAttack;
 
     public Warrior(String name, double maxHealth, int attack, int defense, int rage, String weapon){
         super(name, maxHealth, attack, defense);
 
         this.rage = rage;
         this.weapon = weapon;
+        this.lifeWeapon = 100;
+        this.startAttack = getAttack();
     }
 
+    public int getStartAttack() {
+        return startAttack;
+    }
 
     public String getWeapon() {
         return weapon;
@@ -61,11 +67,30 @@ public class Warrior extends Character {
                 rage =100;
                 healthWarrior = healthWarrior - 25;
                 setHealth(healthWarrior);
+                lifeWeapon -=10;
                 return true;
                 }
 
     }
 }
+
+    public void setLifeWeapon(int lifeWeapon) {
+        this.lifeWeapon = lifeWeapon;
+    }
+
+    public  boolean brokenWeapon (){
+        if (lifeWeapon >=1){
+            return false;
+        }
+        else {
+            if (lifeWeapon ==0){
+                setAttack(1);
+            }
+            return true;
+
+        }
+    }
+
 
 }
 
